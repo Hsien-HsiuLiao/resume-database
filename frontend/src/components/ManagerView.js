740792f925f789047539d7d6c0196f9eb929c22b
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-11-27 05:38:54 -0800

    create components

---
+++ frontend/src/components/ManagerView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ManagerView = () => {
+    return (
+        <div>
+            <h1>Available resumes</h1>
+
+            <li>candidate 1: (resume text)
+            (radio button to select interest)
+            </li>
+        </div>
+    )
+}
