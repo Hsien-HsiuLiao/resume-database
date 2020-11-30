d2fe715d7bade224ff84a124803d56a90f97b498
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-11-30 07:54:27 -0800

    add forms

--- frontend/src/components/CandidateView.js
+++ frontend/src/components/CandidateView.js
@@ -7,13 +7,42 @@ export const CandidateView = () => {
 
             <h3>Please fill out the fields below to submit your resume</h3>
 
+            <form>
+                <label for="fname">First name:</label><br />
+                <input type="text" id="fname" name="fname"></input>
+                <br />
+                <label for="lname">Last name:</label><br />
+                <input type="text" id="lname" name="lname"></input>
+                <br />
+                <label for="resume">Resume:</label><br />
+                <textarea name="message" rows="10" cols="30">
+                
+                    <input type="text" id="resume" name="resume"></input>
+                </textarea>
+                <br />
+                <label for="email">Enter your email:</label>
+                <input type="email" id="email" name="email"></input>
+                <br />
+                <label for="myfile">Or select a file to upload resume:</label>
+                <input type="file" id="myfile" name="myfile"></input>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
             <div>Input resume: </div>
             <div>Input e-mail:</div>
             <div>(submit button)</div>
             <p />
             <div>Status: (pending, accepted/rejected)</div>
+            
             <div>(if accepted) Please select interview time (submit button)</div>
-
+            <form>
+                <label for="appt">Select a time:</label>
+                <input type="time" id="appt" name="appt"></input>
+            </form>
+            <form>
+                <label for="interviewDate">Date:</label>
+                <input type="date" id="interviewDate" name="interviewDate"></input>
+            </form>
         </div>
     )
 }
