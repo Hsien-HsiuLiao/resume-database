495d22786f61dbf7c3ce1bfc9079e24eedbe71b0
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-12-02 09:42:01 -0800

    update form

--- frontend/src/components/CandidateView.js
+++ frontend/src/components/CandidateView.js
@@ -14,15 +14,16 @@ export const CandidateView = () => {
                 <label for="lname">Last name:</label><br />
                 <input type="text" id="lname" name="lname"></input>
                 <br />
+                <label for="email">Enter your email:</label>
+                <input type="email" id="email" name="email"></input>
+                <br />
                 <label for="resume">Resume:</label><br />
                 <textarea name="message" rows="10" cols="30">
                 
                     <input type="text" id="resume" name="resume"></input>
                 </textarea>
                 <br />
-                <label for="email">Enter your email:</label>
-                <input type="email" id="email" name="email"></input>
-                <br />
+                
                 <label for="myfile">Or select a file to upload resume:</label>
                 <input type="file" id="myfile" name="myfile"></input>
                 <br />
