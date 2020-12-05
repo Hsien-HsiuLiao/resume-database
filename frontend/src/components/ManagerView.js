ee419adcf8db6cf50cfdcae0e8dd844589288de0
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-12-05 06:31:15 -0800

    update view

--- frontend/src/components/ManagerView.js
+++ frontend/src/components/ManagerView.js
@@ -3,17 +3,37 @@ import React from 'react';
 export const ManagerView = () => {
     return (
         <div>
-            <h1>Available resumes</h1>
+            <h1>Welcome, (manager) Available resumes</h1>
 
             <li>candidate 1: (resume text)
             </li>
             <form action="isInterested()">
-  <input type="checkbox" id="interested" name="interested" value="Interested">
-        </input>
-  <label for="interested">Interested</label><br />
-  <input type="submit" value="Submit">
-      </input>
-</form>
+                <input type="checkbox" id="interested" name="interested" value="Interested">
+                </input>
+                <label for="interested">Interested</label><br />
+                <input type="submit" value="Submit">
+                </input>
+            </form>
+
+            <li>candidate 2: (resume text)
+            </li>
+            <form action="isInterested()">
+                <input type="checkbox" id="interested" name="interested" value="Interested">
+                </input>
+                <label for="interested">Interested</label><br />
+                <input type="submit" value="Submit">
+                </input>
+            </form>
+
+            <li>candidate 3: (resume text)
+            </li>
+            <form action="isInterested()">
+                <input type="checkbox" id="interested" name="interested" value="Interested">
+                </input>
+                <label for="interested">Interested</label><br />
+                <input type="submit" value="Submit">
+                </input>
+            </form>
         </div>
     )
 }
