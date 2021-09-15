c17d93b749fb2eb290d6166325782131e0e726a2
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2021-09-14 18:21:42 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      Register as a Hiring manager or job seeker
+      Hiring manager fields: account address, name, company
+      job seeker fields: name
+      
       <ManagerView />
       <CandidateView />
 
