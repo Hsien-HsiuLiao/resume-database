ee71282a8a4f9ae46a3587faf7ababf70fdf01dc
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-02-28 16:50:35 -0800

    add contract

---
+++ contracts/resume-db.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Resume-db {
+  /* variables
+  canididate name
+  candidate email
+  candidate eth account
+  bool candidate invited to interview
+
+
+  */
+  
+  //constructor
+
+  /*functtions
+  register as candidte
+  register as hiring mgr
+  get list of candidates for mgr
+  invite candidate to interview
+  offere candidate job offer
+
+  */
+  
+  
+}
